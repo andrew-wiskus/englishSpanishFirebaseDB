5c6d3e9e188acea209371d8e46489f5aa457a12c
Drew Wiskus <andrewwiskus@gmail.com>
2016-11-12 14:38:41 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Regex/phantomjs. 
+personal/experimental use only
+
+
+#13thousandlines of arrays.. containting arrays w/ up to 8 indexs..
+##600 just verb conjs
+##1k nouns
+###todo: adjetives, phrases, idioms.
+###then: map out best sql database structure and push
---
+++ server/public/scripts/firebaseConfig.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDot00e8Pl7VuJw91EWpOnlHMLqCET676s",
+  authDomain: "lingolu-8090f.firebaseapp.com",
+  databaseURL: "https://lingolu-8090f.firebaseio.com",
+  storageBucket: "",
+  messagingSenderId: "957046884682"
+};
+firebase.initializeApp(config);
